a96076199b5a9f0028ab95976bc0be8ac81843b5
agustina ohan <agustina.oh@gmail.com>
2021-03-02 14:42:55 +0100

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #create
+
+  #read all
+  get 'tasks', to: 'tasks#index'
+
+  #update
+  get 'task/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  #read  one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #delete
+
 end
